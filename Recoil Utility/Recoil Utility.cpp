d959fa49ae44f4fa94e2d4f0ae638d3d7c366c0b
Eric Maynard <eric.maynard@stonybrook.edu>
2016-05-13 14:03:18 -0400

    Created Memory Manager

--- Recoil Utility/Recoil Utility.cpp
+++ Recoil Utility/Recoil Utility.cpp
@@ -1,10 +1,37 @@
-// Recoil Utility.cpp : Defines the entry point for the console application.
-//
+//A simple utility to create log files readable by my SmartCross project
+//format of the log files should be:
+
+//END
+//AK-47
+//timestamp,x1,y1
+//timestamp,x2,y2
+//...
+//timestamp,x30,y30
+//OUT
+//timestampSettled
+//END
+//M4A1-S
+//timestamp,x1,y1
+//timestamp,x2,y2
+//...
+//timestamp,x20,y20
+//OUT
+//timestampSettled
+//END
 
 #include "stdafx.h"
+#include <chrono>
+#include "MemoryManager.h"
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+long getTime(){
+	long ms = chrono::duration_cast<chrono::milliseconds>(
+		chrono::system_clock::now().time_since_epoch()).count();
+	return ms;
+}
+
+int main()
 {
 	return 0;
 }
