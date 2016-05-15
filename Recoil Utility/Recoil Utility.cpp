d4aa531b2c9cc9e25ca71e17c9ff580a7dab0f95
Eric Maynard <eric.maynard@stonybrook.edu>
2016-05-15 13:41:46 -0400

    Reads ammo instead of shotsFired. Logs punch vector, not crosshair x/y

--- Recoil Utility/Recoil Utility.cpp
+++ Recoil Utility/Recoil Utility.cpp
@@ -39,8 +39,8 @@ These variables are kept volatile to improve performance:
 */
 volatile int shots;
 unsigned long long timeMS = 69;
-volatile int x;
-volatile int y;
+volatile float x;
+volatile float y;
 volatile bool running = false;
 volatile bool mutex = false;
 MemoryManager * manager;
@@ -51,10 +51,11 @@ dwShots: offset from local player address showing amount of shots fired
 dwPunch: offset from local player address showing current aimpunch
 dwLocal: offset of the local player address from the client
 */
-DWORD dwShots = 0x0000A2B0;
-DWORD dwPunch = 0x00003018;
 DWORD dwLocal = 0x00A844DC;
-
+DWORD dwActive = 0x00002EE8;
+DWORD dwList = 0x04A9F8D4;
+DWORD dwClip = 0x000031F4;
+DWORD dwPunch = 0x00003018;
 
 /*
 These DWORDs are calculated internally, don't worry about changing
@@ -68,8 +69,10 @@ void updateTime(){
 }
 
 void readShots(){
-	myBase = manager->Read<DWORD>(client + dwLocal);
-	shots = manager->Read<int>(myBase + dwShots);
+	DWORD dwLocalPlayer = manager->Read<DWORD>(client + dwLocal);
+	DWORD dwActiveWeapon = manager->Read<DWORD>(dwLocalPlayer + dwActive);
+	DWORD dwWeaponEntity = manager->Read<DWORD>(client + dwList + ((dwActiveWeapon & 0xFFF) - 1) * 0x10);
+	shots = manager->Read<int>(dwWeaponEntity + dwClip);
 }
 
 void readPunch(){
@@ -94,8 +97,10 @@ This is hard-coded for 1920 x 1080
 todo: fix that.
 */
 void calculate(){
-	x = 960 - (21.3333333 * (punch.vec[1]));//positive punchvec1 pulls aim to the left
-	y = 540 + (12         * (punch.vec[0]));//positive punchvec0 pulls aim down...?
+	x = punch.vec[1];
+	y = punch.vec[0];
+	//x = 960 - (21.3333333 * (punch.vec[1]));//positive punchvec1 pulls aim to the left
+	//y = 540 + (12         * (punch.vec[0]));//positive punchvec0 pulls aim down...?
 }
 
 int main()
@@ -118,7 +123,8 @@ int main()
 	thread aThread(updateShots);
 	//cout << "END" << "\n";
 	int lastShots = -1;
-	while (1){
+	bool logging = true;
+	while (logging){
 		if (shots != lastShots){
 			mutex = true;
 			readPunch();
@@ -127,6 +133,18 @@ int main()
 			lastShots = shots;
 			mutex = false;
 		}
+		else if (shots == 0){
+			do{
+				readPunch();
+				calculate();
+				Sleep(1);
+			} while (x != 0.0 || y != 0.0);
+			log();
+			logging = false;
+		}
+	}
+	while (1){
+		Sleep(100);
 	}
 	
 	running = false;//disable threads
